8040d1024fe334c678ac9a2f582bbac9d25259c9
Will Utterback <uwill08@gmail.com>
2020-12-13 05:21:20 -0600

    Implemented mega function generatePassword - debugging

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
 var pwLetters = [
   "q",
   "w",
@@ -52,94 +51,99 @@ var pwSpecial = [
 ];
 //, "\", "|", ":", ";", ",", "<", ".", ">", "?"]
 
-var pwLength = prompt("How many characters do you want? (At least 8 characters and no more than 128 characters)");
 
-if (pwLength < 8 || pwLength > 128) {
-  alert("You didn't select the proper range of characters");
-} else {
-  var pwLowerCase = confirm("Do you want lowercase characters?");
-  var pwUpperCase = confirm("Do you want uppercase characters?");
-  var pwSpecial = confirm("Do you want special-characters?");
-  var pwNumeric = confirm("Do you want numbers?");
-};
+function generatePassword() {
 
-//What happens when you confirm these cases?
-if ((pwLowerCase && pwUpperCase && pwSpecial && pwNumeric) == false) {
-  alert("At least one character type should be selected.");
-}
+  var pwLength = prompt("How many characters do you want? (At least 8 characters and no more than 128 characters)");
+  if (pwLength < 8 || pwLength > 128) {
+    alert("You didn't select the proper range of characters");
+  } else {
+    var pwLowerCase = confirm("Do you want lowercase characters?");
+    var pwUpperCase = confirm("Do you want uppercase characters?");
+    var pwSpecial = confirm("Do you want special-characters?");
+    var pwNumeric = confirm("Do you want numbers?");
+  };
+  //What happens when you confirm these cases?
+  if ((pwLowerCase && pwUpperCase && pwSpecial && pwNumeric) == false) {
+    alert("At least one character type should be selected.");
+  }
 
-if (pwLowerCase === true) {
-  alert("LowerCase true")
-}
+  // ... all
 
-if (pwUpperCase === true) {
+  else if (pwUpperCase && pwSpecial && pwNumeric && pwLowerCase === true) {
 
-}
+  }
 
-if (pwSpecial === true) {
+  // ... 3
 
-}
+  else if (pwLowerCase && pwUpperCase && pwSpecial === true) {
 
-if (pwNumeric === true) {
+  }
 
-}
+  else if (pwLowerCase && pwUpperCase && pwNumeric === true) {
 
-// ... 2
+  }
 
-if (pwLowerCase && pwUpperCase === true) {
+  else if (pwLowerCase && pwNumeric && pwSpecial == true) {
 
-}
+  }
 
-if (pwLowerCase && pwSpecial === true) {
+  else if (pwUpperCase && pwSpecial && pwNumeric === true) {
 
-}
+  }
 
-if (pwUpperCase && pwSpecial === true) {
+  // ... 2
 
-}
+  else if (pwLowerCase && pwUpperCase === true) {
 
-if (pwLowerCase && pwNumeric === true) {
+  }
 
-}
+  else if (pwLowerCase && pwSpecial === true) {
 
-if (pwUpperCase && pwNumeric === true) {
+  }
 
-}
+  else if (pwUpperCase && pwSpecial === true) {
 
-if (pwSpecial && pwNumeric === true) {
+  }
 
-}
+  else if (pwLowerCase && pwNumeric === true) {
 
-// ... 3
+  }
 
-if (pwLowerCase && pwUpperCase && pwSpecial === true) {
+  else if (pwUpperCase && pwNumeric === true) {
 
-}
+  }
 
-if (pwLowerCase && pwUpperCase && pwNumeric === true) {
+  else if (pwSpecial && pwNumeric === true) {
 
-}
+  }
 
-if (pwLowerCase && pwNumeric && pwSpecial == true) {
+  // ... 1
 
-}
+  else if (pwLowerCase === true) {
+    alert("LowerCase true")
+  }
 
-if (pwUpperCase && pwSpecial && pwNumeric === true) {
+  else if (pwUpperCase === true) {
 
-}
+  }
 
-// ... all
+  else if (pwSpecial === true) {
 
-if (pwUpperCase && pwSpecial && pwNumeric && pwLowerCase === true) {
+  }
 
-}
+  else if (pwNumeric === true) {
+    alert("pwNumeric true")
+  }
+
+}; //end of generatePassword function
 
 //loop section
-for (var i = 0; i < pwLength; i++) {
-  //looping through arrays with password choices
-};
+//for (var i = 0; i < pwLength; i++) {
+//looping through arrays with password choices
+//};
 
-// Write password to the #password input
+// Write password to the #password input ***** must implement generatePassword function
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
