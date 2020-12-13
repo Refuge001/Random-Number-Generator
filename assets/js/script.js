02db3e99e8d94933df4540ab3096fd1aa319c3d0
Will Utterback <uwill08@gmail.com>
2020-12-13 03:28:15 -0600

    Special characters array broke, commented out the break and after, basic javascript format written

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var pwLength = prompt("How many characters do you want? (At least 8 characters and no more than 128 characters)");
+var pwCharacterType = prompt("Do you want (l)owercase, (u)ppercase, (n)umeric, and/or (s)pecial-characters?");
+var pwLetters = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
+var pwSpecial = ["!", "@", "#", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "+", "[", "{", "]", "}"]
+//, "\", "|", ":", ";", ",", "<", ".", ">", "?"]
+// input validation?
 
 // Write password to the #password input
 function writePassword() {
